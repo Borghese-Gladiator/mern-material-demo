93d8ca91ac18cfd1802e47df997afddbcc8b333d
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-13 22:15:31 -0500

    rewrite Signin to be like GitHub login
    added @material-ui/lab for Alert component

--- client/src/components/auth/Signin.js
+++ client/src/components/auth/Signin.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
+import { signin } from '../../utils/api-auth.js';
+import auth from '../../utils/auth-helper';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import { Redirect } from 'react-router-dom';
+// Material UI components
+import Link from '@material-ui/core/Link';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import Icon from '@material-ui/core/Icon';
-import { withStyles } from '@material-ui/core/styles';
-import auth from '../../utils/auth-helper';
-import { Redirect } from 'react-router-dom';
-import { signin } from '../../utils/api-auth.js';
+import Paper from '@material-ui/core/Paper';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Container from '@material-ui/core/Container';
+// Material UI Lab component
+import Alert from '@material-ui/lab/Alert';
 
 const styles = theme => ({
 	card: {
@@ -24,17 +31,39 @@ const styles = theme => ({
 	},
 	title: {
 		marginTop: theme.spacing.unit * 2,
-		color: theme.palette.openTitle
+		color: theme.palette.primary.dark
 	},
 	textField: {
 		marginLeft: theme.spacing.unit,
 		marginRight: theme.spacing.unit,
 		width: 300
-	},
-	submit: {
-		margin: 'auto',
-		marginBottom: theme.spacing.unit * 2
-	}
+  },
+  
+  wrapper: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    paddingBottom: theme.spacing(2)
+  },
+  formContainer: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  marginTop: {
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 });
 
 class Signin extends Component {
@@ -79,51 +108,84 @@ class Signin extends Component {
 		}
 
 		return (
-			<Card className={classes.card}>
-				<CardContent>
-					<Typography type="headline" component="h2" className={classes.title}>
-						Sign In
-					</Typography>
-					<TextField
-						id="email"
-						type="email"
-						label="Email"
-						className={classes.textField}
-						value={this.state.email}
-						onChange={this.handleChange('email')}
-						margin="normal"
-					/>
-					<br />
-					<TextField
-						id="password"
-						type="password"
-						label="Password"
-						className={classes.textField}
-						value={this.state.password}
-						onChange={this.handleChange('password')}
-						margin="normal"
-					/>
-					<br />{' '}
-					{this.state.error && (
-						<Typography component="p" color="error">
-							<Icon color="error" className={classes.error}>
-								error
-							</Icon>
-							{this.state.error}
-						</Typography>
-					)}
-				</CardContent>
-				<CardActions>
-					<Button
-						color="primary"
-						variant="contained"
-						onClick={this.clickSubmit}
-						className={classes.submit}
-					>
-						Submit
-					</Button>
-				</CardActions>
-			</Card>
+      <Container component="main" maxWidth="xs">
+        <Paper className={classes.wrapper}>
+          <div className={classes.formContainer}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography variant="subtitle1">
+              Sign in to App
+            </Typography>
+            {this.state.error && (
+              <Alert className={classes.marginTop} severity="error">{this.state.error}</Alert>
+            )}
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label={"Username or Email"}
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={this.state.email}
+                onChange={this.handleChange('email')}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label={"Password"}
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={this.state.password}
+                onChange={this.handleChange('password')}
+              />
+              <Box display="flex" justifyContent="flex-end">
+                <Link to="/forgotpassword" variant="body2">
+                  Forgot password?
+                </Link>
+              </Box>
+              <Button
+                type="button"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                onClick={this.clickSubmit}
+              >
+                {"Sign In"}
+              </Button>
+            </form>
+          </div>
+        </Paper>
+        <Paper>
+          <Box mt={2} p={1} display="flex" justifyContent="center">
+            <Typography variant="body2" style={{margin:'5px'}}>
+              {"New to App? "}
+              <Link to="/signup" variant="body2" style={{margin:'5px'}}>
+                {"Create an account"}
+              </Link>
+            </Typography>{" "}
+          </Box>
+        </Paper>
+        <Box mt={8}>
+          <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+              Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+          </Typography>
+        </Box>
+      </Container>
 		);
 	}
 }
