9122c07f6c9eeef510df2917572920f4212fe57c
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-13 22:14:31 -0500

    remove minheight

--- client/src/components/_layouts/NoLayout.js
+++ client/src/components/_layouts/NoLayout.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NoLayout({ children }) {
   return (
     <>
-      <main style={{minHeight: '85vh'}}>
+      <main>
         {children}
       </main>
     </>
