698d0fc868eb8d63dc7fa3b6203a19638a35279a
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-17 18:49:26 -0500

    add NotFound 404 route

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 // components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import NotFound from './components/NotFound';
 import Home from './components/Home';
 import PrivateRoutes from './components/auth/PrivateRoutes';
 import Signin from './components/auth/Signin';
@@ -21,8 +22,9 @@ function Routes() {
 					<Route path="/signup" component={Signup} />
 					<Route path="/signin" component={Signin} />
 					<Route path="/password_reset" component={ForgotPassword} />
+					<Route component={NotFound} />
 				</Switch>
-			<Footer />
+ 			<Footer />
 		</div>
 	);
 }
