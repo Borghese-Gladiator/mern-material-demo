4f85c4a74689102baaeaa06fd4a9165366832405
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-17 05:55:10 -0500

    fix unused warning

--- client/src/components/auth/Signin.js
+++ client/src/components/auth/Signin.js
@@ -12,7 +12,6 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Container from '@material-ui/core/Container';
-import MaterialLink from '@material-ui/core/Link';
 // Material UI Lab component
 import Alert from '@material-ui/lab/Alert';
 // utils
